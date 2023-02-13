ef66a1290c7205ad66c92e95bf31b2748dd6caf5
haril <songkg7@gmail.com>
2023-02-13 17:04:41 +0900

    feat: Notice and exception handling

--- src/jekyll.ts
+++ src/jekyll.ts
@@ -1,13 +1,22 @@
-import {TFile} from "obsidian"
+import {Notice, TFile} from "obsidian"
 
-export function convertToJekyll() {
-    copyMarkdownFile()
-    removeDoubleSquareBracketsInFiles()
-    // copy image to jeykll image folder
+export async function convertToJekyll() {
+    try {
+        copyMarkdownFile()
+        removeDoubleSquareBracketsInFiles()
+        // copy image to jeykll image folder
 
-    // delete copy file
+        // FIXME: deleteCopiedMarkdownFile() is not working
+        // 삭제하지 않고 복사해서 처리, 처리 중에 파일이 삭제되지 않고 delete 상태만 false 에서 true 로 바뀌는 것을 확인
+        deleteCopiedMarkdownFile()
 
-    // 원본 파일을 published 폴더로 이동
+        // 원본 파일을 published 폴더로 이동
+
+
+        new Notice('Jekyll conversion complete.')
+    } catch (e) {
+        new Notice('Jekyll conversion failed.')
+    }
 }
 
 function removeDoubleSquareBracketsInFiles() {
@@ -22,8 +31,17 @@ function removeDoubleSquareBracketsInFiles() {
 function copyMarkdownFile() {
     let markdownFiles = this.app.vault.getMarkdownFiles()
     markdownFiles.forEach(async (file: TFile) => {
-        console.log(file)
-        // .md to _copy.md
         await this.app.vault.copy(file, file.path.replace(/.md$/, '_copy.md'))
     })
+}
+
+export function deleteCopiedMarkdownFile() {
+    console.log("deleteCopiedMarkdownFile start")
+    let markdownFiles = this.app.vault.getMarkdownFiles()
+    markdownFiles.forEach(async (file: TFile) => {
+        if (file.path.includes('_copy.md')) {
+            await this.app.vault.delete(file)
+        }
+    })
+    console.log("deleteCopiedMarkdownFile end")
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile} from 'obsidian'
 import {DEFAULT_SETTINGS, O2PluginSettings, O2SettingTab} from "./settings"
 import {O2Modal} from "./o2Modal"
-import {convertToJekyll} from "./jekyll"
+import {convertToJekyll, deleteCopiedMarkdownFile} from "./jekyll"
 
 export default class O2Plugin extends Plugin {
     settings: O2PluginSettings
@@ -11,26 +11,6 @@ export default class O2Plugin extends Plugin {
     async onload() {
         await this.loadSettings()
 
-        // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon('dice', 'O2', (evt: MouseEvent) => {
-            // Called when the user clicks the icon.
-            new Notice('This is a notice!')
-        })
-        // Perform additional things with the ribbon
-        ribbonIconEl.addClass('my-plugin-ribbon-class')
-
-        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-        const statusBarItemEl = this.addStatusBarItem()
-        statusBarItemEl.setText('O2 status bar')
-
-        // This adds a simple command that can be triggered anywhere
-        this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Open test sample modal (simple)',
-            callback: () => {
-                new O2Modal(this.app).open()
-            }
-        })
         // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
             id: 'sample-editor-command',
@@ -82,8 +62,16 @@ export default class O2Plugin extends Plugin {
         this.addCommand({
             id: 'test-command',
             name: 'Test Command',
+            callback: () => convertToJekyll()
+        })
+
+        this.addCommand({
+            id: 'check-command',
+            name: 'check current file',
             callback: () => {
-                convertToJekyll()
+                let markdownFiles = this.app.vault.getMarkdownFiles()
+                deleteCopiedMarkdownFile()
+                console.log(markdownFiles)
             }
         })
 
@@ -99,7 +87,6 @@ export default class O2Plugin extends Plugin {
         // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
         this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000))
     }
-
 
     onunload() {
 
