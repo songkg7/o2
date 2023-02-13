01d6e5ff3449e1ae002cbbb9a4d4d2d875ef6d15
haril <songkg7@gmail.com>
2023-02-13 13:49:10 +0900

    feat: move file

--- src/main.ts
+++ src/main.ts
@@ -1,85 +1,104 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
-import {DEFAULT_SETTINGS, O2PluginSettings, O2SettingTab} from "./settings";
-import {O2Modal} from "./o2Modal";
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian'
+import {DEFAULT_SETTINGS, O2PluginSettings, O2SettingTab} from "./settings"
+import {O2Modal} from "./o2Modal"
 
 export default class O2Plugin extends Plugin {
-	settings: O2PluginSettings;
+    settings: O2PluginSettings
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'O2', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // This creates an icon in the left ribbon.
+        const ribbonIconEl = this.addRibbonIcon('dice', 'O2', (evt: MouseEvent) => {
+            // Called when the user clicks the icon.
+            new Notice('This is a notice!')
+        })
+        // Perform additional things with the ribbon
+        ribbonIconEl.addClass('my-plugin-ribbon-class')
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('O2 status bar');
+        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+        const statusBarItemEl = this.addStatusBarItem()
+        statusBarItemEl.setText('O2 status bar')
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open test sample modal (simple)',
-			callback: () => {
-				new O2Modal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new O2Modal(this.app).open();
-					}
+        // This adds a simple command that can be triggered anywhere
+        this.addCommand({
+            id: 'open-sample-modal-simple',
+            name: 'Open test sample modal (simple)',
+            callback: () => {
+                new O2Modal(this.app).open()
+            }
+        })
+        // This adds an editor command that can perform some operation on the current editor instance
+        this.addCommand({
+            id: 'sample-editor-command',
+            name: 'Sample editor command',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                console.log(editor.getSelection())
+                editor.replaceSelection('Sample Editor Command')
+            }
+        })
+        // This adds a complex command that can check whether the current state of the app allows execution of the command
+        this.addCommand({
+            id: 'open-sample-modal-complex',
+            name: 'Open sample modal (complex)',
+            checkCallback: (checking: boolean) => {
+                // Conditions to check
+                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView)
+                if (markdownView) {
+                    // If checking is true, we're simply "checking" if the command can be run.
+                    // If checking is false, then we want to actually perform the operation.
+                    if (!checking) {
+                        new O2Modal(this.app).open()
+                    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+                    // This command will only show up in Command Palette when the check function returns true
+                    return true
+                }
+            }
+        })
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new O2SettingTab(this.app, this));
+        // move the file in ready directory to published directory
+        this.addCommand({
+            id: 'move-file-to-published',
+            name: 'Move file to published',
+            checkCallback: (checking: boolean) => {
+                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView)
+                if (markdownView) {
+                    if (!checking) {
+                        const file = markdownView.file
+                        const from = this.settings.from
+                        const to = this.settings.to
+                        const toPath = file.path.replace(from, to)
+                        this.app.vault.rename(file, toPath)
+                    }
+                    return true
+                }
+            }
+        })
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new O2SettingTab(this.app, this))
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+        // Using this function will automatically remove the event listener when this plugin is disabled.
+        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+            console.log('click', evt)
+        })
 
-	onunload() {
+        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000))
+    }
 
-	}
+    onunload() {
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings)
+    }
 }
 
--- src/settings.ts
+++ src/settings.ts
@@ -1,61 +1,61 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import O2Plugin from "./main";
+import {App, PluginSettingTab, Setting} from "obsidian"
+import O2Plugin from "./main"
 
 export interface O2PluginSettings {
-	from: string;
-	to: string;
+    from: string;
+    to: string;
 }
 
 export const DEFAULT_SETTINGS: O2PluginSettings = {
-	from: 'ready',
-	to: 'published'
+    from: 'ready',
+    to: 'published'
 }
 
 export class O2SettingTab extends PluginSettingTab {
-	plugin: O2Plugin;
+    plugin: O2Plugin
 
-	constructor(app: App, plugin: O2Plugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: O2Plugin) {
+        super(app, plugin)
+        this.plugin = plugin
+    }
 
-	display(): void {
-		this.containerEl.empty();
-		this.containerEl.createEl('h2', {
-			text: 'Settings for O2 plugin.',
-		});
-		this.addReadyDirectorySetting();
-		this.addPublishedDirectorySetting();
-		this.containerEl.createEl('h2', {
-			text: 'Advanced settings',
-		})
-	}
+    display(): void {
+        this.containerEl.empty()
+        this.containerEl.createEl('h2', {
+            text: 'Settings for O2 plugin.',
+        })
+        this.addReadyDirectorySetting()
+        this.addPublishedDirectorySetting()
+        this.containerEl.createEl('h2', {
+            text: 'Advanced settings',
+        })
+    }
 
-	private addReadyDirectorySetting() {
-		new Setting(this.containerEl)
-			.setName('Ready directory')
-			.setDesc('The directory where documents ready to be published will be placed.')
-			.addText(text => text
-				.setPlaceholder('Enter directory name')
-				.setValue(this.plugin.settings.from)
-				.onChange(async (value) => {
-					console.log('From: ' + value);
-					this.plugin.settings.from = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    private addReadyDirectorySetting() {
+        new Setting(this.containerEl)
+            .setName('Ready directory')
+            .setDesc('The directory where documents ready to be published will be placed.')
+            .addText(text => text
+                .setPlaceholder('Enter directory name')
+                .setValue(this.plugin.settings.from)
+                .onChange(async (value) => {
+                    console.log('From: ' + value)
+                    this.plugin.settings.from = value
+                    await this.plugin.saveSettings()
+                }))
+    }
 
-	private addPublishedDirectorySetting() {
-		new Setting(this.containerEl)
-			.setName('Published directory')
-			.setDesc('The directory where publishing will complete and the documents will be moved.')
-			.addText(text => text
-				.setPlaceholder('Enter directory name')
-				.setValue(this.plugin.settings.to)
-				.onChange(async (value) => {
-					console.log('To: ' + value);
-					this.plugin.settings.to = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    private addPublishedDirectorySetting() {
+        new Setting(this.containerEl)
+            .setName('Published directory')
+            .setDesc('The directory where publishing will complete and the documents will be moved.')
+            .addText(text => text
+                .setPlaceholder('Enter directory name')
+                .setValue(this.plugin.settings.to)
+                .onChange(async (value) => {
+                    console.log('To: ' + value)
+                    this.plugin.settings.to = value
+                    await this.plugin.saveSettings()
+                }))
+    }
 }
