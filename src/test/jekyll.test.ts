c3c64ea71bd480858bfdb93f568f7c59c87442da
haril <songkg7@gmail.com>
2023-02-17 22:31:42 +0900

    test: basic test skeleton

--- src/test/jekyll.test.ts
+++ src/test/jekyll.test.ts
@@ -1,9 +1,17 @@
 import {convertToJekyll} from "../jekyll"
 
-describe('markdown 파일이 jekyll 로 변환되는지 테스트 demo', () => {
+describe("jekyll", () => {
     // convertToJekyll()
 
     test('1 + 1 = 2', () => {
         expect(1 + 1).toBe(2)
     })
+
+    it.todo("should read a file in ready directory only")
+
+    it.todo("![NOTE] title & contents should be converted to content {: .prompt-info}")
+})
+
+describe("ready directory 안에 .md 파일을 읽을 수 있는지 테스트", () => {
+
 })
