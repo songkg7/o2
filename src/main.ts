d01dc639f5f5d41f40c5c651f4d9a4c50c072869
haril <songkg7@gmail.com>
2023-02-24 21:09:48 +0900

    fix: remove unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ export default class O2Plugin extends Plugin {
         await this.loadSettings();
 
         this.addCommand({
-            id: 'o2-converting',
+            id: 'converting',
             name: 'converting',
             checkCallback: (checking: boolean) => {
                 // TODO: init jekyll from to folder
@@ -24,14 +24,6 @@ export default class O2Plugin extends Plugin {
 
         this.addSettingTab(new O2SettingTab(this.app, this));
 
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-            console.log('click', evt);
-        });
-
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
     }
 
     onunload() {
