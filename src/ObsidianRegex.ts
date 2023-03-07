df0135d7df69d3b3418cdf4a8317663873fa6a93
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2023-03-07 20:47:24 +1100

    refactor: replace ObsidianRegex namespace

--- src/ObsidianRegex.ts
+++ src/ObsidianRegex.ts
@@ -1,7 +1,5 @@
-// FIXME: This is a temporary way to mimic Java's enum.
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace ObsidianRegex {
-    export const IMAGE_LINK = /!\[\[(.*?)]]/g;
-    export const DOCUMENT_LINK = /(?<!!)\[\[(.*?)]]/g;
-    export const CALLOUT = /> \[!(.*)].*?\n(>.*)/ig;
-}
+export const ObsidianRegex = {
+    IMAGE_LINK: /!\[\[(.*?)]]/g,
+    DOCUMENT_LINK: /(?<!!)\[\[(.*?)]]/g,
+    CALLOUT: /> \[!(.*)].*?\n(>.*)/ig,
+} as const;
