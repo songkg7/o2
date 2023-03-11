8f2f4f56bd78c9099eadd720b7210f60c000f317
haril <songkg7@gmail.com>
2023-03-11 18:37:52 +0900

    test: ResourceLinkConverter convert function

--- src/tests/ResourceLinkConverter.test.ts
+++ src/tests/ResourceLinkConverter.test.ts
@@ -1,6 +1,10 @@
-import { extractResourceNames } from "../jekyll/ResourceLinkConverter";
+import { extractResourceNames, ResourceLinkConverter } from "../jekyll/ResourceLinkConverter";
 
 jest.mock('obsidian', () => ({}), { virtual: true });
+jest.mock('fs', () => ({
+    mkdirSync: jest.fn(),
+    copyFile: jest.fn()
+}));
 
 describe("extract image name", () => {
 
@@ -16,3 +20,16 @@ describe("extract image name", () => {
 
 });
 
+describe("convert called", () => {
+    const converter = new ResourceLinkConverter(
+        '2023-01-01-post-mock',
+        'assets',
+        'test',
+        'attachments',
+        'assets'
+    );
+
+    it('should return converted post', () => {
+        expect(converter.convert(`![[test.png]]`)).toEqual(`![image](/assets/2023-01-01-post-mock/test.png)`);
+    });
+});
