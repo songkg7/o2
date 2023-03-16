cb26aa599f8d6604b0aa57b5a84a94ff70a77424
haril <songkg7@gmail.com>
2023-03-16 13:44:15 +0900

    feat: o2-67 support comments syntax

---
+++ src/jekyll/CommentsConverter.ts
@@ -0,0 +1,12 @@
+import { Converter } from "../core/Converter";
+import { ObsidianRegex } from "../ObsidianRegex";
+
+export class CommentsConverter implements Converter {
+
+    public convert(input: string): string {
+        return input.replace(ObsidianRegex.COMMENT, (match, comments) => {
+            return `<!--${comments}-->`;
+        });
+    }
+
+}
