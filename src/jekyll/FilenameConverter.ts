4625c2f0159af6df67b369a31e66ee17e81d8132
L2HYUNN <donghyuni98@gmail.com>
2024-02-11 18:15:05 +0900

    fix: fix a bug FilenameConverter cant convert file containing Korean characters

--- src/jekyll/FilenameConverter.ts
+++ src/jekyll/FilenameConverter.ts
@@ -1,7 +1,8 @@
 export class FilenameConverter {
   convert(filename: string): string {
-    return filename.replace('.md', '')
+    return filename
+      .replace('.md', '')
       .replace(/\s/g, '-')
-      .replace(/[^a-zA-Z0-9-]/g, '');
+      .replace(/[^a-zA-Z0-9-\uAC00-\uD7A3]/g, '');
   }
 }
