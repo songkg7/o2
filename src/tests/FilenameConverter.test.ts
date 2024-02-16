f2d75920f0fa8aa8b1ec8b4c116196ad135a7949
L2HYUNN <donghyuni98@gmail.com>
2024-02-16 22:40:52 +0900

    test: add a test that FilenameConverter convert Korean characters

--- src/tests/FilenameConverter.test.ts
+++ src/tests/FilenameConverter.test.ts
@@ -13,14 +13,21 @@ describe('FilenameConverter', () => {
     expect(filename).toEqual('This-is-Obsidian');
   });
 
+  it('should convert Korean characters', () => {
+    const filename = filenameConverter.convert('이것은 옵시디언입니다.md');
+    expect(filename).toEqual('이것은-옵시디언입니다');
+  });
+
   it.each([
     ['This is Obsidian.md', 'This-is-Obsidian'],
     ['This is Obsidian!!.md', 'This-is-Obsidian'],
-    ['Obsidian, awesome note-taking tool.md', 'Obsidian-awesome-note-taking-tool'],
+    [
+      'Obsidian, awesome note-taking tool.md',
+      'Obsidian-awesome-note-taking-tool',
+    ],
     ['Obsidian(Markdown Editor).md', 'ObsidianMarkdown-Editor'],
   ])('should remove special characters', (filename, expected) => {
     const result = filenameConverter.convert(filename);
     expect(result).toEqual(expected);
   });
-
 });
