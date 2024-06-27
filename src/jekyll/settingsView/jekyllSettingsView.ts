5314acee238a86a47834eb023374eab4281a28df
haril <songkg7@gmail.com>
2024-06-27 13:44:45 +0900

    .

---
+++ src/jekyll/settingsView/jekyllSettingsView.ts
@@ -0,0 +1,34 @@
+import { Setting } from 'obsidian';
+
+export function jekyllSettingsView() {
+  addJekyllPathSetting.call(this);
+  addJekyllRelativeResourcePathSetting.call(this);
+}
+
+function addJekyllPathSetting() {
+  const jekyllSetting = this.plugin.settings.jekyllSetting();
+  new Setting(this.containerEl)
+    .setName('Jekyll path')
+    .setDesc('The absolute path where Jekyll is installed.')
+    .addText(text => text
+      .setPlaceholder('Enter path')
+      .setValue(jekyllSetting.jekyllPath)
+      .onChange(async (value) => {
+        jekyllSetting.jekyllPath = value;
+        await this.plugin.saveSettings();
+      }));
+}
+
+function addJekyllRelativeResourcePathSetting() {
+  const jekyllSetting = this.plugin.settings.jekyllSetting();
+  new Setting(this.containerEl)
+    .setName('Relative resource path')
+    .setDesc('The relative path where resources are stored. (default: assets/img)')
+    .addText(text => text
+      .setPlaceholder('Enter path')
+      .setValue(jekyllSetting.jekyllRelativeResourcePath)
+      .onChange(async (value) => {
+        jekyllSetting.jekyllRelativeResourcePath = value;
+        await this.plugin.saveSettings();
+      }));
+}
