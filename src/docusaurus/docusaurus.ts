e33a9c0a342e7963ed77a16b4857d6ae20007c4d
haril <songkg7@gmail.com>
2024-06-27 17:51:37 +0900

    .

--- src/docusaurus/docusaurus.ts
+++ src/docusaurus/docusaurus.ts
@@ -3,14 +3,22 @@ import { renameMarkdownFile } from '../utils';
 import { Contents } from '../core/Converter';
 import { convertWikiLink } from '../jekyll/WikiLinkConverter';
 import { convertFootnotes } from '../jekyll/FootnotesConverter';
+import { convertDocusaurusCallout } from '../jekyll/CalloutConverter';
+import { convertComments } from '../jekyll/CommentsConverter';
 
 export const convertToDocusaurus = async (plugin: O2Plugin) => {
   // get file name in ready folder
   const markdownFiles = await renameMarkdownFile(plugin);
   for (const file of markdownFiles) {
     const contents: Contents = await plugin.app.vault.read(file);
-    (convertFootnotes(
-      convertWikiLink(contents),
-    ));
+    convertComments(
+      convertDocusaurusCallout(
+        convertFootnotes(
+          convertWikiLink(
+            contents,
+          ),
+        ),
+      ),
+    );
   }
 };
--- src/jekyll/CommentsConverter.ts
+++ src/jekyll/CommentsConverter.ts
@@ -6,5 +6,6 @@ export class CommentsConverter implements Converter {
   public convert(input: string): string {
     return input.replace(ObsidianRegex.COMMENT, (match, comments) => `<!--${comments}-->`);
   }
-
 }
+
+export const convertComments = (input: string) => input.replace(ObsidianRegex.COMMENT, (match, comments) => `<!--${comments}-->`);
