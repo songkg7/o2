782a74870bd49e65644cbefa67e468c61c2c9e5f
haril <songkg7@gmail.com>
2024-06-27 13:35:00 +0900

    Revert "."
    This reverts commit e417a854d1c7199913d2f712e96c04e6b499f562.

--- src/settings.ts
+++ src/settings.ts
@@ -129,7 +129,7 @@ export class O2SettingTab extends PluginSettingTab {
     this.addReadyFolderSetting();
     this.addBackupFolderSetting();
     this.addAttachmentsFolderSetting();
-    this.addTargetPlatformPathSetting();
+    this.addJekyllPathSetting();
     this.addJekyllRelativeResourcePathSetting();
     this.containerEl.createEl('h2', {
       text: 'Features',
@@ -209,12 +209,11 @@ export class O2SettingTab extends PluginSettingTab {
         }));
   }
 
-  private addTargetPlatformPathSetting() {
+  private addJekyllPathSetting() {
     const o2Settings = this.plugin.settings.o2Settings();
     new Setting(this.containerEl)
-      .setName('Target workspace path')
-      .setDesc('The absolute path where target platform workspace is located.')
-      .setTooltip('e.g. /Users/username/Documents/your-blog')
+      .setName('Jekyll path')
+      .setDesc('The absolute path where Jekyll is installed.')
       .addText(text => text
         .setPlaceholder('Enter path')
         .setValue(o2Settings.jekyllPath)
