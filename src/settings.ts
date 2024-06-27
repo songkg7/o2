1217afb2a04f02e002ddcba164588cdb389ff339
haril <songkg7@gmail.com>
2024-06-27 13:58:05 +0900

    Revert "."
    This reverts commit 5314acee238a86a47834eb023374eab4281a28df.

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,5 @@
 import { App, Notice, PluginSettingTab, Setting } from 'obsidian';
 import O2Plugin from './main';
-import { jekyllSettingsView } from './jekyll/settingsView/jekyllSettingsView';
 
 export interface O2PluginSettings {
   targetPlatform: string;
@@ -126,14 +125,12 @@ export class O2SettingTab extends PluginSettingTab {
     this.containerEl.createEl('h2', {
       text: 'Path Settings',
     });
-
     this.platformDropdownSetting();
     this.addReadyFolderSetting();
     this.addBackupFolderSetting();
     this.addAttachmentsFolderSetting();
-
-    jekyllSettingsView();
-
+    this.addJekyllPathSetting();
+    this.addJekyllRelativeResourcePathSetting();
     this.containerEl.createEl('h2', {
       text: 'Features',
     });
@@ -208,6 +205,34 @@ export class O2SettingTab extends PluginSettingTab {
         .setValue(jekyllSetting.isEnableBanner)
         .onChange(async (value) => {
           jekyllSetting.isEnableBanner = value;
+          await this.plugin.saveSettings();
+        }));
+  }
+
+  private addJekyllPathSetting() {
+    const jekyllSetting = this.plugin.settings.jekyllSetting();
+    new Setting(this.containerEl)
+      .setName('Jekyll path')
+      .setDesc('The absolute path where Jekyll is installed.')
+      .addText(text => text
+        .setPlaceholder('Enter path')
+        .setValue(jekyllSetting.jekyllPath)
+        .onChange(async (value) => {
+          jekyllSetting.jekyllPath = value;
+          await this.plugin.saveSettings();
+        }));
+  }
+
+  private addJekyllRelativeResourcePathSetting() {
+    const jekyllSetting = this.plugin.settings.jekyllSetting();
+    new Setting(this.containerEl)
+      .setName('Relative resource path')
+      .setDesc('The relative path where resources are stored. (default: assets/img)')
+      .addText(text => text
+        .setPlaceholder('Enter path')
+        .setValue(jekyllSetting.jekyllRelativeResourcePath)
+        .onChange(async (value) => {
+          jekyllSetting.jekyllRelativeResourcePath = value;
           await this.plugin.saveSettings();
         }));
   }
